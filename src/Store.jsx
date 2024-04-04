27ae7dac4b31319d0ecb33f80e64191d8c1e929a
shahalmoideen <=>
2024-04-04 12:48:12 +0530

    branch commit

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,19 +1,20 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { accountreducer } from "./assets/Features/Account/AccountSlice";
+import {configureStore} from "@reduxjs/toolkit"
+import accountreducer  from "./assets/Features/Account/AccountSlice";
 import { customerReducer } from "./assets/Features/Customer/CustomerSlice";
-import { thunk } from "redux-thunk";
-import {composeWithDevTools} from "@redux-devtools/extension"
+
 
 
 
 //-----------------------------------------------------//
 //store//
 
-const rootReducer = combineReducers({
-  account: accountreducer,
-  customer: customerReducer,
-});
 
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
+
+const store = configureStore({
+  reducer:{
+    account: accountreducer,
+    customer: customerReducer,
+  }
+})
 
 export default store
--- src/assets/Features/Account/AccountSlice.jsx
+++ src/assets/Features/Account/AccountSlice.jsx
@@ -1,3 +1,6 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+
 const initialState = {
     balance: 0,
     loan: 0,
@@ -6,77 +9,110 @@ const initialState = {
   };
 
 
-  export  function accountreducer(state = initialState, action) {
-    switch (action.type) {
-      //new recomented pattern
-      //balence==paylaod
-      case "account/deposit":
-        return { ...state, balance: state.balance + action.balance ,isLoading:false };
+const accountSlice = createSlice({
+  name:"account",
+  initialState:initialState,
+  reducers:{
+    deposit(state,action){
+      state.balance = state.balance + action.payload
+    },
+    witdraw(state,action){
+      state.balance -= action.payload 
+    },
+    requestLoan(state,actiongit){
+      if(state.loan>0) return;
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance = state.balance + action.payload.amount;
+    },
+    payloan(state,action){
+      state.loan = 0
+      state.loanPurpose=""
+      state.balance= state.balance-state.loan
+    },
+
+  },
+});
+// console.log(accountSlice);
+
+
+
+export const {deposit,witdraw, requestLoan, payloan} = accountSlice.actions
+
+export default accountSlice.reducer;
+
+
+//   export  function accountreducer(state = initialState, action) {
+//     switch (action.type) {
+//       //new recomented pattern
+//       //balence==paylaod
+//       case "account/deposit":
+//         return { ...state, balance: state.balance + action.balance ,isLoading:false };
   
-      case "account/witdraw":
-        return { ...state, balance: state.balance - action.balance };
+//       case "account/witdraw":
+//         return { ...state, balance: state.balance - action.balance };
   
-      case "account/requestLoan":
-        if (state.loan > 0) return state;
+//       case "account/requestLoan":
+//         if (state.loan > 0) return state;
   
-        return {
-          ...state,
-          loan: action.balance.amount,
-          loanPurpose: action.balance.purpose,
-          balance: state.balance + action.balance.amount,
-        };
+//         return {
+//           ...state,
+//           loan: action.balance.amount,
+//           loanPurpose: action.balance.purpose,
+//           balance: state.balance + action.balance.amount,
+//         };
   
-      case "account/payloan":
-        return {
-          ...state,
-          loan: 0,
-          loanPurpose: "",
-          balance: state.balance - state.loan,
-        };
-        case "account/convertingCurrency":
-          return{
-           ...state,
-           isLoading:true,
-          }
-      default:
-        return state;
-    }
-  }
+//       case "account/payloan":
+//         return {
+//           ...state,
+//           loan: 0,
+//           loanPurpose: "",
+//           balance: state.balance - state.loan,
+//         };
+//         case "account/convertingCurrency":
+//           return{
+//            ...state,
+//            isLoading:true,
+//           }
+//       default:
+//         return state;
+//     }
+//   }
 
-  //-----------------------------------------------------//
-// action creators for account reducer
+//   //-----------------------------------------------------//
+// // action creators for account reducer
 
-export function deposit(amount,currency) {
-    console.log(amount,currency)
-    if(currency === 'INR') return {type: "account/deposit",balance:amount};
+// export function deposit(amount,currency) {
+//     console.log(amount,currency)
+//     if(currency === 'INR') return {type: "account/deposit",balance:amount};
 
-    return async function (dispatch,getState){
-      //API Call
-      dispatch({type:"account/convertingCurrency"})
-      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=INR`) ;
+//     return async function (dispatch,getState){
+//       //API Call
+//       dispatch({type:"account/convertingCurrency"})
+//       const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=INR`) ;
 
-      const data = await res.json();
-      console.log(data)
-      const converted = data.rates.INR;
+//       const data = await res.json();
+//       console.log(data)
+//       const converted = data.rates.INR;
 
-      dispatch({type:"account/deposit",balance:converted})
+//       dispatch({type:"account/deposit",balance:converted})
 
       
-    }
-  }
+//     }
+//   }
 
   
- export  function witdraw(amount) {
-    return { type: "account/witdraw", balance: amount };
-  }
+//  export  function witdraw(amount) {
+//     return { type: "account/witdraw", balance: amount };
+//   }
   
- export  function requestLoan(amount) {
-    return {
-      type: "account/requestLoan",
-      balance: { amount: 50000, purpose: "buy a car" },
-    };
-  }
+//  export  function requestLoan(amount) {
+//     return {
+//       type: "account/requestLoan",
+//       balance: { amount: 50000, purpose: "buy a car" },
+//     };
+//   }
   
- export  function payloan(amount) {
-    return { type: "account/witdraw", balance: amount };
-  }
+//  export  function payloan(amount) {
+//     return { type: "account/witdraw", balance: amount };
+//   }
